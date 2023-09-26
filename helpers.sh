630de145fb5ec35abf8d031c386a58b4a843fefa
Kirill Tikhanskiy <s8ckerpunch@gmail.com>
2023-09-26 17:24:01 +0300

    add old java versions

--- helpers.sh
+++ helpers.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+function is_java_version_valid {
+    JAVA_VERSION=$1
+    local JAVA_VERSIONS=("21", "20", "19", "17", "11", "8")
+
+    if [[ ! ${JAVA_VERSIONS[@]} =~ $JAVA_VERSION ]]; then
+        echo "Unavailable Java version. You shoud use one of these: ${JAVA_VERSIONS[@]}"
+        exit 1
+    fi
+}
+
 function get_arch {
+    JAVA_VERSION=$1
     ARCH=$(uname -m)
 
     case $ARCH in 
         "x86_64" | "amd64")
-        ARCH="x64";;
+            if [[ $JAVA_VERSION -ge 17 ]]; then
+                ARCH="x64"
+            else
+                ARCH="amd64"
+            fi ;;
         "arm64")
         ARCH="aarch64";;
     esac
@@ -15,25 +30,44 @@ function get_arch {
 }
 
 function get_os {
+    JAVA_VERSION=$1
     OS=$(uname | tr '[:upper:]' '[:lower:]')
 
     case $OS in 
         "linux")
         OS="linux";;
         "darwin")
-        OS="macos";;
+        if [[ $JAVA_VERSION -ge 17 ]]; then
+            OS="macos"
+        else
+            OS="darwin"
+        fi 
     esac
 
     echo $OS
 }
 
 function build_java_url {
-   local JDK_FULL_VERSION=$1
+   local JAVA_VERSION=$1
    local OS=$2
    local ARCH=$3
 
-   echo "https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${JDK_FULL_VERSION}/graalvm-community-jdk-${JDK_FULL_VERSION}_${OS}-${ARCH}_bin.tar.gz"
+   case $JAVA_VERSION in
+        "21")
+        JAVA_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${JAVA_VERSION}/graalvm-community-jdk-${JAVA_VERSION}.0.0_${OS}-${ARCH}_bin.tar.gz";;
+        "20")
+        JAVA_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${JAVA_VERSION}/graalvm-community-jdk-${JAVA_VERSION}.0.2_${OS}-${ARCH}_bin.tar.gz";;
+        "19")
+        JAVA_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java19-${OS}-${ARCH}-22.3.0.tar.gz";;
+        "17")
+        JAVA_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${JAVA_VERSION}/graalvm-community-jdk-${JAVA_VERSION}.0.8_${OS}-${ARCH}_bin.tar.gz";;
+        "11")
+        JAVA_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.1/graalvm-ce-java11-${OS}-${ARCH}-22.3.1.tar.gz";;
+        "8")
+        JAVA_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-21.2.0/graalvm-ce-java8-${OS}-${ARCH}-21.2.0.tar.gz";; 
+    esac
 
+    echo $JAVA_URL 
 }
 
 function build_maven_url {
--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 source "./helpers.sh"
 
-JDK_FULL_VERSION=$1
+JAVA_VERSION=$1
 MAVEN_FULL_VERSION=$2
 MAVEN_MAJOR_VERSION=${MAVEN_FULL_VERSION:0:1}
 
-ARCH=$(get_arch)
-OS=$(get_os)
+is_java_version_valid $JAVA_VERSION || exit 1
+
+ARCH=$(get_arch $JAVA_VERSION)
+OS=$(get_os $JAVA_VERSION)
 
 # CREATE DIRS FOR JAVA & MAVEN
 mkdir -p env/jdk && mkdir -p env/maven
 
 # INSTALL JAVA
-JAVA_URL=$(build_java_url $JDK_FULL_VERSION $OS $ARCH)
+JAVA_URL=$(build_java_url $JAVA_VERSION $OS $ARCH)
+
 download_and_unpack $JAVA_URL "jdk" 
 
 # INSTALL MAVEN
